2c3ff31b7479813156049f7e337775492326afec
Lubin Mendez <mendezlubin@yahoo.com>
2025-06-19 21:40:40 -0700

    refactor: organize header HTML and switch to kebab-case classes

--- js/code.js
+++ js/code.js
@@ -1,4 +1,16 @@
 // alert("conected");
 
+const hamburguerButton = document.querySelector(".hamburguer-button");
+const mobileMenu = document.getElementById("mobile-menu-nav");
 
+hamburguerButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+});
+
+// Closes the menu when any link inside it is clicked
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("open");
+  });
+});
 
